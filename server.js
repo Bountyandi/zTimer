c6ab8af0b2d0c1333a2e578b235cd2bce488bbc7
Andy <bountyandi@gmail.com>
2017-11-22 02:27:37 +0200

    modify for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
 
@@ -9,4 +10,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(3000);
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
