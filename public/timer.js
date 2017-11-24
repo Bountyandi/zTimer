edd31302dd924381b18718b9e694142170b30132
Andy <bountyandi@gmail.com>
2017-11-24 02:00:08 +0200

    add dbClick functionality

--- public/timer.js
+++ public/timer.js
@@ -1,33 +1,66 @@
 var timerIntervalId;
-var date = new Date(START_DATE);
 var isTicking = false;
+var clickCount = 0;
 
 //CONSTANTS
 var DEFAULT_TEMPLATE = '00:00:00';
-var ROOT = 'root';
 var TITLE = 'zTimer';
 var ONE_SECOND = 1000; //ms
 var START_DATE = 'May 15, 1991 00:00:00';
+var date = new Date(START_DATE);
+;
 
-//or document.body.onclick
-window.onclick = function (ev) {
-  if (isTicking) {
-    stopTimer();
-  } else {
-    startTimer();
-  }
-};
-
-function startTimer() {
+window.onload = function () {
+  //set defaults UI data
   updateHTML(DEFAULT_TEMPLATE);
   updateTitle(DEFAULT_TEMPLATE);
 
+  document.body.onclick = clicksHandler;
+};
+
+function singleClick() {
+  isTicking ? pauseTimer() : startTimer()
+};
+
+function doubleClick() {
+  stopTimer();
+  resetTimer();
+  startTimer();
+};
+
+function clicksHandler(event) {
+  event.stopPropagation();
+
+  clickCount++;
+  if (clickCount === 1) {
+    singleClickTimer = setTimeout(function () {
+      clickCount = 0;
+
+      singleClick();
+    }, 200);
+  } else if (clickCount === 2) {
+    clearTimeout(singleClickTimer);
+    clickCount = 0;
+
+    doubleClick();
+  }
+};
+
+
+function startTimer() {
   isTicking = true;
   drawBackground();
-  resetTimer();
+
   timerIntervalId = setInterval(tickHandler, ONE_SECOND);
 };
 
+function pauseTimer() {
+  isTicking = false;
+  drawBackground();
+  clearInterval(timerIntervalId);
+};
+
+//for now same like pauseTimer()
 function stopTimer() {
   isTicking = false;
   drawBackground();
@@ -41,14 +74,14 @@ function tickHandler() {
   var minutes = getMinutes();
   var seconds = getSeconds();
 
-  var currentTime = `${hours}:${minutes}:${seconds}`;
+  var currentTime = hours + ':' + minutes + ':' + seconds;
 
   updateHTML(currentTime);
   updateTitle(currentTime);
 };
 
 function resetTimer() {
-  date = new Date(START_DATE)
+  date = new Date(START_DATE);
 };
 
 function makeTick() {
@@ -56,22 +89,22 @@ function makeTick() {
 };
 
 function updateTitle(value) {
-  document.title = `${value} - ${TITLE}`;
+  document.title = value + ' - ' + TITLE;
 }
 
 function getHours() {
   var hours = date.getHours();
-  return hours < 10 ? `0${hours}` : hours;
+  return hours < 10 ? '0' + hours : hours;
 };
 
 function getMinutes() {
   var minutes = date.getMinutes();
-  return minutes < 10 ? `0${minutes}` : minutes;
+  return minutes < 10 ? '0' + minutes : minutes;
 };
 
 function getSeconds() {
   var seconds = date.getSeconds();
-  return seconds < 10 ? `0${seconds}` : seconds;
+  return seconds < 10 ? '0' + seconds : seconds;
 };
 
 function updateHTML(time) {
@@ -79,9 +112,6 @@ function updateHTML(time) {
 };
 
 function drawBackground() {
-  var root = document.getElementById(
-    ROOT
-  );
   if (isTicking) {
     document.body.style.backgroundColor = 'lightgreen';
   } else {
