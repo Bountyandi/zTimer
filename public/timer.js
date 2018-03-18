d7bde90a6917b5401ddd7b6424cbce60b0ad7d26
Andy <bountyandi@gmail.com>
2018-03-19 00:41:47 +0200

    Add Space, Enter and Esc buttons support

--- public/timer.js
+++ public/timer.js
@@ -8,113 +8,125 @@ var TITLE = 'zTimer';
 var ONE_SECOND = 1000; //ms
 var START_DATE = 'May 15, 1991 00:00:00';
 var date = new Date(START_DATE);
-;
+
 
 window.onload = function () {
-  //set defaults UI data
-  updateHTML(DEFAULT_TEMPLATE);
-  updateTitle(DEFAULT_TEMPLATE);
+    //set defaults UI data
+    updateHTML(DEFAULT_TEMPLATE);
+    updateTitle(DEFAULT_TEMPLATE);
 
-  document.body.onclick = clicksHandler;
+    document.body.onclick = clicksHandler;
+    document.body.onkeyup = _onkeyupHandler;
 };
 
 function singleClick() {
-  isTicking ? pauseTimer() : startTimer()
+    isTicking ? pauseTimer() : startTimer()
 };
 
 function doubleClick() {
-  stopTimer();
-  resetTimer();
-  startTimer();
+    stopTimer();
+    resetTimer();
+    startTimer();
 };
 
-function clicksHandler(event) {
-  event.stopPropagation();
+function _onkeyupHandler(e) {
+    if ( ( e.keyCode == 13 ) || ( e.keyCode == 32 ) ) { // Enter and Space
+        clicksHandler(e);
+    } else if (e.keyCode == 27) { // Esc
+        clicksHandler(e, true);
+    }
 
-  clickCount++;
-  if (clickCount === 1) {
-    singleClickTimer = setTimeout(function () {
-      clickCount = 0;
+}
 
-      singleClick();
-    }, 200);
-  } else if (clickCount === 2) {
-    clearTimeout(singleClickTimer);
-    clickCount = 0;
+function clicksHandler(e, imperativeReload) {
+    e.stopPropagation();
+    debugger
+    clickCount++;
+    if (clickCount == 1) {
+        singleClickTimerId = setTimeout(function () {
+            clickCount = 0;
 
-    doubleClick();
-  }
+            singleClick();
+        }, 200);
+    }
+    // else
+    if (( clickCount == 2 ) || imperativeReload) {
+        clearTimeout(singleClickTimerId);
+        clickCount = 0;
+
+        doubleClick();
+    }
 };
 
 
 function startTimer() {
-  isTicking = true;
-  drawBackground();
+    isTicking = true;
+    drawBackground();
 
-  timerIntervalId = setInterval(tickHandler, ONE_SECOND);
+    timerIntervalId = setInterval(tickHandler, ONE_SECOND);
 };
 
 function pauseTimer() {
-  isTicking = false;
-  drawBackground();
-  clearInterval(timerIntervalId);
+    isTicking = false;
+    drawBackground();
+    clearInterval(timerIntervalId);
 };
 
 //for now same like pauseTimer()
 function stopTimer() {
-  isTicking = false;
-  drawBackground();
-  clearInterval(timerIntervalId);
+    isTicking = false;
+    drawBackground();
+    clearInterval(timerIntervalId);
 };
 
 function tickHandler() {
-  makeTick();
+    makeTick();
 
-  var hours = getHours();
-  var minutes = getMinutes();
-  var seconds = getSeconds();
+    var hours = getHours();
+    var minutes = getMinutes();
+    var seconds = getSeconds();
 
-  var currentTime = hours + ':' + minutes + ':' + seconds;
+    var currentTime = hours + ':' + minutes + ':' + seconds;
 
-  updateHTML(currentTime);
-  updateTitle(currentTime);
+    updateHTML(currentTime);
+    updateTitle(currentTime);
 };
 
 function resetTimer() {
-  date = new Date(START_DATE);
+    date = new Date(START_DATE);
 };
 
 function makeTick() {
-  date.setSeconds(date.getSeconds() + 1);
+    date.setSeconds(date.getSeconds() + 1);
 };
 
 function updateTitle(value) {
-  document.title = value + ' - ' + TITLE;
+    document.title = value + ' - ' + TITLE;
 }
 
 function getHours() {
-  var hours = date.getHours();
-  return hours < 10 ? '0' + hours : hours;
+    var hours = date.getHours();
+    return hours < 10 ? '0' + hours : hours;
 };
 
 function getMinutes() {
-  var minutes = date.getMinutes();
-  return minutes < 10 ? '0' + minutes : minutes;
+    var minutes = date.getMinutes();
+    return minutes < 10 ? '0' + minutes : minutes;
 };
 
 function getSeconds() {
-  var seconds = date.getSeconds();
-  return seconds < 10 ? '0' + seconds : seconds;
+    var seconds = date.getSeconds();
+    return seconds < 10 ? '0' + seconds : seconds;
 };
 
 function updateHTML(time) {
-  document.getElementById('timer').innerHTML = time;
+    document.getElementById('timer').innerHTML = time;
 };
 
 function drawBackground() {
-  if (isTicking) {
-    document.body.style.backgroundColor = 'lightgreen';
-  } else {
-    document.body.style.backgroundColor = 'indianred';
-  }
+    if (isTicking) {
+        document.body.style.backgroundColor = 'lightgreen';
+    } else {
+        document.body.style.backgroundColor = 'indianred';
+    }
 };
